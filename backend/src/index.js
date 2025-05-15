ba7897dfa85377575784994ea89d7fb8b1a96615
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-05-16 02:31:10 +0530

    1.27

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+const app = express();
+app.use(cookieParser());
+const PORT = process.env.PORT;
+app.use(express.json())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT: " + PORT);
+  connectDB()
+});
