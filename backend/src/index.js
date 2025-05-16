6a9a2ff348561035f2d371f54c7e56c133fd58b7
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-05-17 02:14:43 +0530

    SettingsPage

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,18 +3,28 @@ import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+
 const app = express();
+
 app.use(cookieParser());
-const PORT = process.env.PORT;
-app.use(express.json())
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log("Server is running on PORT: " + PORT);
-  connectDB()
+  connectDB();
 });
